f08174ca0f83cf0383626efdf3ce23b8de9107bb
Breno Santiago <83508587+Santlago@users.noreply.github.com>
2023-04-26 00:39:59 +0000

    att ontacliente

---
+++ 2CursoJava/projeto-banco/src/ContaCliente.java
@@ -0,0 +1,36 @@
+public class ContaCliente {
+    //Atributos
+    private String nome;
+    private String tipo;
+    private double saldo;
+
+    //Métodos
+    
+    
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
+}
