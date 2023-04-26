6e87b27d1c02a4edde98e985eb58bdb5c201d771
Breno Santiago <83508587+Santlago@users.noreply.github.com>
2023-04-26 01:40:40 +0000

    finalizado

--- 2CursoJava/projeto-banco/src/ContaCliente.java
+++ 2CursoJava/projeto-banco/src/ContaCliente.java
@@ -1,11 +1,65 @@
+import java.util.Scanner;
+
 public class ContaCliente {
     //Atributos
     private String nome;
     private String tipo;
     private double saldo;
 
+    //Construtores
+    public ContaCliente(String nome, String tipo, double saldo) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+
     //Métodos
-  
+    public void operacao() {
+        Scanner input = new Scanner(System.in);
+
+        int n = 0;
+        while (n != 4) {
+
+            System.out.println("\nOperações:\n\n1- Consultar saldos\n2- Receber valor\n3- Transferir valor\n4- Sair\n\nDigite a opção desejada:");
+            n = input.nextInt();
+
+            switch (n) {
+                case 1:
+                    System.out.println("\nSaldo atual: " + this.getSaldo());
+                    break;
+                case 2:
+                    System.out.println("\nDigite o valor a receber: ");
+                    double v = input.nextDouble();
+                    this.recebe(v);
+                    System.out.println("\nSaldo atualizado: " + this.getSaldo());
+                    break;
+                case 3:
+                    System.out.println("\nDigite o valor a transferir: ");
+                    double v2 = input.nextDouble();
+                    this.transfere(v2);
+                    System.out.println("\nSaldo atualizado: " + this.getSaldo());
+                    break;
+                case 4:
+                    System.out.println("\nAtendimento finalizado!");
+                    break;
+                default:
+                    System.out.println("\nOpção inválida");
+                    break;
+            }
+        }
+    }
+
+    public void transfere(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        } else {
+            System.out.println("Não há saldo suficiente para fazer essa transferência.");
+        }
+    }
+
+    public void recebe(double valor) {
+        this.saldo += valor;
+    }
     
     //Getters e Setters
     public String getNome() {
--- 2CursoJava/projeto-banco/src/Main.java
+++ 2CursoJava/projeto-banco/src/Main.java
@@ -4,13 +4,8 @@ public class Main {
     
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        ContaCliente jacqueline = new ContaCliente("Jacqueline", "Corrente", 2500);
 
-        ContaCliente jacqueline = new ContaCliente();
-        jacqueline.setNome("Jacqueline");
-        jacqueline.setTipo("Corrente");
-        jacqueline.setSaldo(2500);
-
-        System.out.println("Operações:\n\n1- Consultar saldos\n2- Receber valor\n3- Transferir valor\n4- Sair\n\nDigite a opção desejada:");
+        jacqueline.operacao();
     }
 }
