409d827bf139ccb5df100e4849dedab4eec01910
Santlago <breno.santiago.8@gmail.com>
2023-09-01 17:14:48 -0300

    att

--- FormacaoJavaOO/4curso-consumindo-api/screenmatch/src/principal/PrincpalComBusca.java
+++ FormacaoJavaOO/4curso-consumindo-api/screenmatch/src/principal/PrincpalComBusca.java
@@ -1,14 +1,20 @@
 package principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class PrincpalComBusca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://foo.com/"))
-            .build();
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=da68d1f1"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
