a7d3eb8166358d33e17f655eff6d84d821af6d52
Santlago <breno.santiago.8@gmail.com>
2024-01-30 12:28:28 -0300

    att

--- FormacaoNextJs/1curso/projeto/pages/index.jsx
+++ FormacaoNextJs/1curso/projeto/pages/index.jsx
@@ -1,9 +1,26 @@
+import React from 'react'
 import Link from 'next/link'
+
+function Title({ children, as }) {
+    const Tag = as;
+    return(
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                }
+            `}</style>
+        </React.Fragment>
+    )
+}
 
 export default function HomePage() {
     return(
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <Title as="h1">Alura Cases - Home Page</Title>
             <Link href='/faq'>
                 Ir para a pagina do FAQ
             </Link>
