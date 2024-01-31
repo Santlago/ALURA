3b8da8ba3f6e3d58045d8150d92a4cf161fc0449
Santlago <breno.santiago.8@gmail.com>
2024-01-31 11:52:20 -0300

    att

--- FormacaoNextJs/1curso/projeto/pages/index.jsx
+++ FormacaoNextJs/1curso/projeto/pages/index.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import Link from 'next/link'
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children, as }) {
-    const Tag = as;
-    return(
-        <React.Fragment>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </React.Fragment>
-    )
-}
+export default HomeScreen;
 
-export default function HomePage() {
-    return(
-        <div>
-            <Title as="h1">Alura Cases - Home Page</Title>
-            <Link href='/faq'>
-                Ir para a pagina do FAQ
-            </Link>
-        </div>
-    )
-}
+// import React from 'react'
+// import Link from 'next/link'
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return(
+//         <React.Fragment>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </React.Fragment>
+//     )
+// }
+
+// export default function HomePage() {
+//     return(
+//         <div>
+//             <Title as="h1">Alura Cases - Home Page</Title>
+//             <Link href='/faq'>
+//                 Ir para a pagina do FAQ
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
