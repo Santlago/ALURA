1290f1c3809df252afee47aedbfcb7e1ef5bfceb
Santlago <breno.santiago.8@gmail.com>
2024-02-02 15:05:15 -0300

    att

--- FormacaoNextJs/1curso/projeto/next.config.js
+++ FormacaoNextJs/1curso/projeto/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     trailingSlash: true,
+    async redirects() {
+        return [
+            {
+                source: '/perguntas',
+                destination: '/faq/',
+                permanent: true,
+            },
+        ]
+    }
 }
